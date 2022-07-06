c4f3a213dae1b3bcba38b182d26b3c5e965c16e9
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 09:09:35 +0530

    [Sharmila] Player1 reaches 100

--- Snake_Ladder_game/Snake.cs
+++ Snake_Ladder_game/Snake.cs
@@ -13,19 +13,30 @@ namespace Snake_Ladder_game
         public void rollingDice()
         {
             Random rand = new Random();
-            player1 = rand.Next(0, 7);
-            int option1 = rand.Next(0, 3);
-
-            switch (option1)
+            while (pos1 != 100)
             {
-                case 0:
-                    break;
-                case 1:
-                    pos1 += player1;
-                    break;
-                case 2:
+                player1 = rand.Next(0, 7);
+                int option1 = rand.Next(0, 3);
+
+                switch (option1)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        pos1 += player1;
+                        break;
+                    case 2:
+                        pos1 -= player1;
+                        break;
+                }
+                if (pos1 < 0)
+                {
+                    pos1 = 0;
+                }
+                if (pos1 > 100)
+                {
                     pos1 -= player1;
-                    break;
+                }
             }
         }
     }
